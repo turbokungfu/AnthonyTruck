f7461dce15f9b7d7929c224bed30d46977462abb
turbokungfu <slashrandazzo@gmail.com>
2023-02-20 09:42:08 -0800

    added page refresh

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -1,83 +1,51 @@
 <script>
 
     let botField = '';
- 
     let name = '';
- 
     let email = '';
- 
     let message = '';
- 
-  
- 
     let serverState;
- 
     $: submitting = false;
  
   
  
     function handleServerResponse(ok, msg) {
- 
       serverState = { ok, msg };
- 
     }
  
   
  
     const handleSubmit = async () => {
- 
       console.log({ email, name, message });
- 
       try {
- 
         submitting = true;
- 
         await fetch(`/.netlify/functions/contact-form-message`, {
- 
           method: 'POST',
- 
           credentials: 'omit',
- 
           headers: {
- 
             'Content-Type': 'application/json',
- 
           },
- 
           body: JSON.stringify({
- 
             botField,
- 
             email,
- 
             name,
- 
             message,
- 
           }),
- 
         });
- 
         submitting = false;
- 
         handleServerResponse(true, '');
- 
+        alert('Message received');
+        if (window.confirm('Do you want to reload the page?')) {
+        location.reload();
+        }
       } catch (error) {
- 
-        console.error(`Eror: ${error}`);
- 
+        console.error(`Error: ${error}`);
         submitting = false;
- 
         handleServerResponse(
- 
           false,
- 
           'Unable to send your message at the moment.  Please try again later.',
- 
         );
- 
       }
- 
     };
  
   </script>
@@ -85,75 +53,40 @@
   
  
   <form on:submit|preventDefault={handleSubmit} class="form-container">
- 
     <input aria-hidden="true" type="hidden" name="bot-field" bind:value={botField} />
- 
     <div>
- 
       <span class="screen-reader-text"><label for="name">Name</label></span>
- 
       <input bind:value={name} required id="name" placeholder="Name" title="Name" type="text" />
- 
     </div>
- 
     <div>
- 
       <span class="screen-reader-text"><label for="email">Email</label></span>
- 
       <input
- 
         bind:value={email}
- 
         required
- 
         id="email"
- 
-        placeholder="blake@example.com"
- 
+        placeholder="customer@example.com"
         title="Email"
- 
         type="email"
- 
       />
- 
     </div>
- 
     <div>
- 
       <span class="screen-reader-text"><label for="message">Message</label></span>
- 
       <textarea
- 
         bind:value={message}
- 
         required
- 
         id="message"
- 
         rows={6}
- 
         placeholder="Write your message here..."
- 
         title="Message"
- 
         type="text"
- 
       />
- 
     </div>
- 
     <div class="button-container">
- 
       <button type="submit" disabled={submitting}> Send</button>
- 
     </div>
- 
     {#if serverState}<p class={!serverState.ok ? 'errorMsg' : ''}>
- 
         {serverState.msg}
- 
       </p>{/if}
- 
   </form>
  
   
